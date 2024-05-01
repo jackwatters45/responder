a0d795f800b0e7d74cf137132557637a7adbf090
Jack Watters <johnwatt@usc.edu>
2024-04-30 23:21:07 -0700

    Basic server actions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,37 @@
 import { Button } from "~/_components/ui/button";
 import { db } from "~/server/db";
 import { users } from "~/server/db/schema";
-import { getUsers } from "~/server/queries";
+import { addUser, getUsers } from "~/server/queries";
 
 export default async function HomePage() {
 	// const router = useRouter();
 	// onClick={) => router.refresh(()}
 	const usersQuery = await getUsers();
 
-	async function addUser() {
-		"use server";
-
-		await db.insert(users).values({
-			name: "User",
-			email: "jackwattersdev@me.com",
-			company: "Company",
-		});
-	}
-
 	return (
 		<div className="p-48">
-			<Button type="button" onClick={addUser}>
-				something that needs to be refreshed
-			</Button>
+			<div className="flex gap-4">
+				<form
+					action={async () => {
+						"use server";
+
+						await addUser();
+					}}
+				>
+					<Button type="submit">add user</Button>
+				</form>
+				<form
+					action={async () => {
+						"use server";
+
+						await addUser();
+					}}
+				>
+					<Button type="submit" variant={"destructive"}>
+						delete
+					</Button>
+				</form>
+			</div>
 			<h1>Users</h1>
 			<ul>
 				{usersQuery.map((user) => (
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,8 +1,21 @@
 import "server-only";
+import { revalidatePath } from "next/cache";
 import { db } from "./db";
+import { users } from "./db/schema";
 
 export async function getUsers() {
 	const usersQuery = await db.query.users.findMany();
 
 	return usersQuery;
+}
+
+export async function addUser() {
+	await db.insert(users).values({
+		name: "User",
+		email: "jackwattersdev@me.com",
+		company: "Company",
+	});
+
+	revalidatePath("/");
+	// redirect("/");
 }
