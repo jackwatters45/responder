6a5a7be620a46ff11ff9d3c9ede04d0a0507c70e
Jack Watters <johnwatt@usc.edu>
2024-05-19 13:03:37 -0700

    new UI components.

--- src/app/_components/ui/textarea.tsx
+++ src/app/_components/ui/textarea.tsx
@@ -1,19 +1,21 @@
+"use client";
+
+import RCTextarea from "rc-textarea";
+import type { TextAreaProps, TextAreaRef } from "rc-textarea";
 import * as React from "react";
 
 import { cn } from "~/lib/utils";
 
-export interface TextareaProps
-	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+const Textarea = React.forwardRef<TextAreaRef, TextAreaProps>(
 	({ className, ...props }, ref) => {
 		return (
-			<textarea
+			<RCTextarea
 				className={cn(
 					"flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
 					className,
 				)}
 				ref={ref}
+				autoSize={true}
 				{...props}
 			/>
 		);
