96cea36f0047126691930701250808cb8d6b4f81
Jack Watters <johnwatt@usc.edu>
2024-05-23 12:36:38 -0700

    Reorganize onboarding components.

---
+++ src/app/_components/filter-card.tsx
@@ -0,0 +1,218 @@
+"use client";
+
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+
+import { Button } from "~/app/_components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+} from "~/app/_components/ui/card";
+import { Input } from "~/app/_components/ui/input";
+import { Label } from "~/app/_components/ui/label";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "~/app/_components/ui/select";
+import { Switch } from "~/app/_components/ui/switch";
+import { Textarea } from "~/app/_components/ui/textarea";
+
+export default function FilterCard({
+	id,
+	removeFilter,
+}: { id: string; removeFilter: (id: string) => void }) {
+	return (
+		<Card>
+			<CardHeader className="space-y-2">
+				<Label htmlFor={`filter.${id}.name`} className="text-xl">
+					Name
+				</Label>
+				<Input
+					id={`filter.${id}.name`}
+					name={`filter.${id}.name`}
+					placeholder="Positive Review, Negative Review, etc."
+				/>
+			</CardHeader>
+			<CardContent className="space-y-8">
+				<div className="space-y-2">
+					<Label>Filter</Label>
+					<div className="flex items-center gap-2 border py-2 px-4 rounded-md">
+						<span>Review is </span>
+						<Select name={`filter.${id}.filter.comparison`}>
+							<SelectTrigger className="w-[200px]">
+								<SelectValue placeholder="Select comparison" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									<SelectLabel>Comparison</SelectLabel>
+									<SelectItem value="==">equal to</SelectItem>
+									<SelectItem value="<=">less than or equal to</SelectItem>
+									<SelectItem value=">=">greater than or equal to</SelectItem>
+									<SelectItem value="<">less than</SelectItem>
+									<SelectItem value=">">greater than</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						<Select name={`filter.${id}.filter.rating`}>
+							<SelectTrigger className="w-[150px]" id="negative-filter">
+								<SelectValue placeholder="Select rating" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									<SelectLabel>Ratings</SelectLabel>
+									<SelectItem value="1">1 - Very Bad</SelectItem>
+									<SelectItem value="2">2 - Bad</SelectItem>
+									<SelectItem value="3">3 - Okay</SelectItem>
+									<SelectItem value="4">4 - Good</SelectItem>
+									<SelectItem value="5">5 - Very Good</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						<span>stars</span>
+					</div>
+				</div>
+				<div className="space-y-2">
+					<Label htmlFor={`filter.${id}.prompt`}>Prompt</Label>
+					<Textarea
+						id={`filter.${id}.prompt`}
+						name={`filter.${id}.prompt`}
+						placeholder="Enter your GPT prompt template"
+					/>
+				</div>
+				<div className="flex items-center gap-2">
+					<Switch
+						id={`filter.${id}.manual-response`}
+						name={`filter.${id}.manual-response`}
+					/>
+					<Label htmlFor={`filter.${id}.manual-response`}>
+						Send email instead of auto-generating replies for negative reviews
+					</Label>
+				</div>
+				<div className="space-y-2">
+					<Label>Response Time</Label>
+					<div className="flex items-center gap-2 border py-2 px-4 rounded-md">
+						<span>Between</span>
+						<Label htmlFor={`filter.${id}.response-time.start`} hidden />
+						<Input
+							id={`filter.${id}.response-time.start`}
+							name={`filter.${id}.response-time.start`}
+							placeholder="start"
+							defaultValue={"30"}
+						/>
+						<span>and</span>
+						<Label htmlFor={`filter.${id}.response-time.end`} hidden />
+						<Input
+							id={`filter.${id}.response-time.end`}
+							name={`filter.${id}.response-time.end`}
+							placeholder="end"
+							defaultValue={"60"}
+						/>
+						<Select defaultValue="minutes" name={`filter.${id}.response-time.unit`}>
+							<SelectTrigger className="w-full">
+								<SelectValue placeholder="Select response range" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value="minutes">Minutes</SelectItem>
+								<SelectItem value="hours">Hours</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
+				</div>
+			</CardContent>
+			<CardFooter>
+				<Button
+					onClick={() => removeFilter(id)}
+					type="button"
+					variant="destructive"
+				>
+					Delete Filter
+				</Button>
+			</CardFooter>
+		</Card>
+	);
+}
+
+export const filter = z.object({
+	id: z.string(),
+	name: z.string({ invalid_type_error: "Invalid Filter" }),
+	filter: z.object({
+		comparison: z.string(),
+		rating: z.string(),
+	}),
+	prompt: z.string(),
+	sendEmail: z.boolean(),
+	responseTime: z.object({
+		range: z.object({
+			start: z.string(),
+			end: z.string(),
+		}),
+		timeUnit: z.string(),
+	}),
+});
+
+const getDefaultFilter = (): Filter => ({
+	id: nanoid(),
+	name: "",
+	filter: {
+		comparison: "",
+		rating: "",
+	},
+	prompt: "",
+	sendEmail: false,
+	responseTime: {
+		range: {
+			start: "30",
+			end: "60",
+		},
+		timeUnit: "minutes",
+	},
+});
+
+export type Filter = z.infer<typeof filter>;
+
+function Filters({
+	filters,
+	removeFilter,
+}: { filters: Filter[]; removeFilter: (id: string) => void }) {
+	const [parent] = useAutoAnimate();
+
+	// const [filters, setFilters] = useState<Filter[]>([getDefaultFilter()]);
+	// const addFilter = () => setFilters((prev) => [...prev, getDefaultFilter()]);
+	// const removeFilter = (id: string) => {
+	// 	if (filters.length < 2) return;
+	// 	setFilters((prev) => prev.filter((filter) => filter.id !== id));
+	// };
+
+	// 	<Button
+	// 	className="mr-auto"
+	// 	type="button"
+	// 	variant="outline"
+	// 	onClick={addFilter}
+	// >
+	// 	+ Add Filter
+	// </Button>
+
+	return (
+		<div className="space-y-8 pt-8">
+			<div className="space-y-2">
+				<div className="text-2xl font-bold">Add Filters</div>
+				<div className=" text-muted-foreground ">
+					Configure your filters based on review rating
+				</div>
+			</div>
+			<div className="grid gap-6" ref={parent}>
+				{filters.map((filter) => (
+					<FilterCard key={filter.id} id={filter.id} removeFilter={removeFilter} />
+				))}
+			</div>
+		</div>
+	);
+}
---
+++ src/app/_components/pricing-card.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState } from "react";
+import { Button, ButtonLabel } from "~/app/_components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "~/app/_components/ui/card";
+import { Input } from "~/app/_components/ui/input";
+import { titleCase } from "~/lib/utils";
+
+type Price = {
+	monthly: number;
+	yearly: number;
+};
+
+interface PricingCardProps {
+	buttonVariant?: "outline" | "secondary";
+	name: string;
+	description: string | React.ReactNode | null;
+	price: Price;
+	bulletPoints: string[];
+	isSelected: boolean;
+	setIsSelected: () => void;
+}
+
+export default function PricingCard({
+	name,
+	description,
+	price,
+	bulletPoints,
+	isSelected,
+	setIsSelected,
+}: PricingCardProps) {
+	return (
+		<Card
+			className="data-[selected=true]:ring-2 data-[selected=true]:ring-muted-foreground"
+			data-selected={isSelected}
+		>
+			<CardHeader className="space-y-4">
+				<CardTitle>{titleCase(name)}</CardTitle>
+				<CardDescription className="space-y-4 text-muted-foreground">
+					{description}
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<div className="pb-4 space-x-1">
+					<span className="text-3xl font-semibold">${price.monthly}</span>
+					<span className="text-muted-foreground text-sm">/month</span>
+				</div>
+				<ul className="ml-6 list-disc [&>li]:mt-2 text-sm ">
+					{bulletPoints.map((point) => (
+						<li key={point}>{point}</li>
+					))}
+				</ul>
+			</CardContent>
+			<CardFooter className="pt-4">
+				<Input
+					type="radio"
+					name="plan"
+					id={name}
+					value={name}
+					className="hidden"
+					onChange={() => setIsSelected()}
+					defaultChecked={isSelected}
+				/>
+				<ButtonLabel
+					role="radio"
+					htmlFor={name}
+					variant={isSelected ? "default" : "outline"}
+					className="w-full"
+				>
+					{isSelected ? "Selected" : "Select"}
+				</ButtonLabel>
+			</CardFooter>
+		</Card>
+	);
+}
+
+function ChoosePlan() {
+	const [selectedPlan, setSelectedPlan] = useState<"free" | "premium">("free");
+
+	return (
+		<div>
+			<div className="space-y-8 pt-8">
+				<div className="space-y-2">
+					<div className="text-2xl font-bold">Choose Your Plan</div>
+					<div className=" text-muted-foreground ">
+						Upgrade to unlock premium features and manage multiple businesses.
+					</div>
+				</div>
+				<div className="grid grid-cols-2 gap-4">
+					<PricingCard
+						name="free"
+						description="For small businesses looking to manage a single location using only basic features."
+						price={{ monthly: 0, yearly: 0 }}
+						bulletPoints={[
+							"Single business",
+							"Default negative and positive review filters",
+							"Automated review responses",
+						]}
+						isSelected={selectedPlan === "free"}
+						setIsSelected={() => setSelectedPlan("free")}
+					/>
+					<PricingCard
+						name="premium"
+						description="For growing businesses looking to manage multiple locations and unlock premium features."
+						price={{ monthly: 5, yearly: 50 }}
+						bulletPoints={[
+							"Multiple Businesses",
+							"Custom review filters",
+							"Automated and manual review responses",
+							// "Detailed analytics and reporting",
+						]}
+						isSelected={selectedPlan === "premium"}
+						setIsSelected={() => setSelectedPlan("premium")}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+}
